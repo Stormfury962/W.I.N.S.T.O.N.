12a80b2cc31b9cfb72a617812609f743aa338815
adamderbala <adamderbala@gmail.com>
2025-04-27 08:00:59 -0400

    fixing typos in posting.js

--- WINSTON-app/src/Posting.js
+++ WINSTON-app/src/Posting.js
@@ -1,7 +1,7 @@
 let posts = [];
 function addpost() {
-    let title = document.getelementbyid("Newposttitle").value;
-    let desc = document.getelementbyid("Newpostdescription").value;
+    let title = document.getElementById("Newposttitle").value;
+    let desc = document.getElementById("Newpostdescription").value;
     if (title === "" || desc === "") {
         alert("Enter in both a title and description");
         return;
@@ -12,21 +12,21 @@ function addpost() {
         questions: []
     };
     posts.push(post);
-    document.getelementbyid("Newposttitle").value = "";
-    document.getelementbyid("Newpostdescription").value = "";
+    document.getElementById("Newposttitle").value = "";
+    document.getElementById("Newpostdescription").value = "";
     showpost();
 }
 function showpost() {
-    let postsdiv = document.getelementbyid("list-posts");
+    let postsdiv = document.getElementById("list-posts");
     postsdiv.innerHTML = "";
     for (let i = 0; i < posts.length; i++) {
-        let div = document.createelement("div");
-        div.classname = "postcard";
+        let div = document.createElement("div");
+        div.className = "postcard";
         div.innerHTML = "<h3>" + posts[i].title + "</h3>" +
                          "<p>" + posts[i].description + "</p>" +
                          "<button onclick='addquestion(" + i + ")'>New Question</button>" +
                          "<div id='q-list-" + i + "'></div>";
-        postsdiv.appendchild(div);
+        postsdiv.appendChild(div);
     }
 }
 function addquestion(postindex) {
@@ -43,20 +43,20 @@ function addquestion(postindex) {
     }
 }
 function showquestions(postindex) {
-    let questiondiv = document.getelementbyid("q-list-" + postindex);
+    let questiondiv = document.getElementById("q-list-" + postindex);
     questiondiv.innerHTML = "";
     for (let j = 0; j < posts[postindex].questions.length; j++) {
-        let question = document.createelement("div");
+        let question = document.createElement("div");
         question.classname = "postcard";
         question.innerHTML = "<p><b>Q: </b>" + posts[postindex].questions[j].title + "</p>" +
                          "<textarea id='ans-input-" + postindex + "-" + j + "' placeholder='Type in an answer'></textarea>" +
                          "<button onclick='submitAns(" + postindex + ", " + j + ")'>Submit</button>" +
                          "<div id='ans-list-" + postindex + "-" + j + "'></div>";
-        questiondiv.appendchild(question);
+        questiondiv.appendChild(question);
     }
 }
 function submitanswer(postidx, questionidx) {
-    let answerarea = document.getelementbyid("answer-input-" + postidx + "-" + questionidx);
+    let answerarea = document.getElementById("answer-input-" + postidx + "-" + questionidx);
     let answer = answerarea.value;
     if (answer.trim() !== "") {
         posts[postidx].questions[questionidx].answers.push(answer);
@@ -67,13 +67,13 @@ function submitanswer(postidx, questionidx) {
     }
 }
 function showanswers(postidx, questionidx) {
-    let answerdiv = document.getelementbyid("ans-list-" + postidx + "-" + questionidx);
+    let answerdiv = document.getElementById("ans-list-" + postidx + "-" + questionidx);
     answerdiv.innerHTML = "";
     let allanswer = posts[postidx].questions[questionidx].answers;
     for (let k = 0; k < allanswer.length; k++) {
-        let a = document.createelement("div");
+        let a = document.createElement("div");
         a.classname = "replyarea";
         a.innertext = allanswer[k];
-        answerdiv.appendchild(a);
+        answerdiv.appendChild(a);
     }
 }
